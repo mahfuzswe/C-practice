50ceb874c700f3c2345673531d66acf927b8f236
Invincible-21 <shantobd1969@gmail.com>
2022-05-27 18:42:46 +0600

    new

---
+++ factorial.c
@@ -0,0 +1,23 @@
+// Print the factorial of a number
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    int fact = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+    printf("Final factorial is: %d\n", fact);
+
+    return 0;
+}
+
+// output:
+// Enter number: 5
+// Final factorial is: 120
