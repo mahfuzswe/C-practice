ad2f1e5375c6c5242cf31dedfa80acfc11189b03
Invincible-21 <shantobd1969@gmail.com>
2022-05-28 18:34:16 +0600

    added new file

--- Properties_of_function.c
+++ Properties_of_function.c
@@ -1,6 +1,32 @@
 // Properties of function:
 // ----------------------
 
-// *Execution always starts from main
-// *A function gets called directly or indirectly from main
+// *Execution always starts from main.
+// *A function gets called directly or indirectly from main.
 // *There can be multiple functions in a program.
+
+// Function Types: 1.Library function -----------> Special function built in C ; Ex. scanf(),printf()
+//                 2.User defined fiunction -----> decleared and defined by programmer
+
+// Passing Arguments:
+// -----------------
+// functions can take value[parameter] and give some value[return value]
+
+// passing arguments:
+// void printHello();
+// void printTable(int n);  <---
+// void sum(int a, int b);  <---
+
+//        Arguments                        vs                              Parameter:
+//        ---------                                                        ----------
+// values taht are passed                                              values in function
+//   in function call                                             declearation and defination
+
+//  used to send value                                                used to receive value
+
+//   actual parameter                                                    formal parameter
+
+// NOTE:
+//  a. Function can only return one value at a time.
+//  b. Changes to parameters in function don't change the values in calling function.
+//  Because a copy of argument is passed to the function.
