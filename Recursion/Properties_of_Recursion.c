9fccf743c9845a5994a368a2b5288eb028b784bb
Invincible-21 <shantobd1969@gmail.com>
2022-06-07 10:25:51 +0600

    updated

---
+++ Recursion/Properties_of_Recursion.c
@@ -0,0 +1,7 @@
+// a. Anything can be done with iteration(loop) , can be done with recursion and vice-versa.
+
+// b. Recursion can sometimes give that most simple solution.
+
+// c. Base case is the condition whcih stops recursion.
+
+// d. Iteration has infinate loop and Recursion has stack overflow.
