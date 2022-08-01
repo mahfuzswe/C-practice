603e4fc6eae99709b918b09b65ca6a290149e6da
Invincible-21 <shantobd1969@gmail.com>
2022-08-02 01:54:24 +0600

    miles to go..

---
+++ Header Files/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include"my_header_file.h"
+
+int main()
+{
+    add(10,50);
+    printf("Sum = %d\n", SUM);
+    printf("Name from header file: %s\n",NAME);
+
+    return 0;
+}
---
+++ Header Files/my_header_file.h
@@ -0,0 +1,10 @@
+#define SUM 10
+#define NAME "SHANTO"
+
+
+void add(int a, int b){
+    int x = a+b;
+    printf("%d + %d = %d\n",a,b,x);
+}
+
+
